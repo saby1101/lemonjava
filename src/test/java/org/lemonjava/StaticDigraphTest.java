8e0eb01525839f5f0d0fcd8a61ec3c76d6b357d6
Sabyasachi Bhattacharjee <saby@stackbox.xyz>
2018-12-23 14:42:40 +0530

    Updated test files

--- src/test/java/org/lemonjava/StaticDigraphTest.java
+++ src/test/java/org/lemonjava/StaticDigraphTest.java
@@ -29,14 +29,23 @@ public class StaticDigraphTest {
         Assert.assertEquals(-2, supplyMap.get(3));
 
         StaticDigraph.ArcMapDouble costMap = graph.createArcMapDouble();
+        StaticDigraph.ArcMapLong costMapLong = graph.createArcMapLong();
         costMap.set(0, 10.5);
         costMap.set(1, 20.1);
         costMap.set(2, 100.0);
         costMap.set(3, 20.0);
+        costMapLong.set(0, 10);
+        costMapLong.set(1, 20);
+        costMapLong.set(2, 100);
+        costMapLong.set(3, 20);
         Assert.assertEquals(10.5, costMap.get(0), DELTA);
         Assert.assertEquals(20.1, costMap.get(1), DELTA);
         Assert.assertEquals(100.0, costMap.get(2), DELTA);
         Assert.assertEquals(20.0, costMap.get(3), DELTA);
+        Assert.assertEquals(10, costMapLong.get(0));
+        Assert.assertEquals(20, costMapLong.get(1));
+        Assert.assertEquals(100, costMapLong.get(2));
+        Assert.assertEquals(20, costMapLong.get(3));
 
         StaticDigraph.ArcMapLong upperMap = graph.createArcMapLong();
         upperMap.set(0, 1);
@@ -49,13 +58,19 @@ public class StaticDigraphTest {
         Assert.assertEquals(3, upperMap.get(3));
 
         StaticDigraph.CostScalingDouble algo = graph.costScalingDouble();
+        StaticDigraph.CostScalingLong algoLong = graph.costScalingLong();
         algo.setCostMap(costMap);
         algo.setUpperMap(upperMap);
         algo.setSupplyMap(supplyMap);
+        algoLong.setCostMap(costMapLong);
+        algoLong.setUpperMap(upperMap);
+        algoLong.setSupplyMap(supplyMap);
 
         MinCostFlowResultType result = algo.run();
+        MinCostFlowResultType result1 = algoLong.run();
 
         Assert.assertEquals(MinCostFlowResultType.OPTIMAL, result);
+        Assert.assertEquals(MinCostFlowResultType.OPTIMAL, result1);
 
         long flow1 = algo.flow(0);
         long flow2 = algo.flow(1);
@@ -67,7 +82,18 @@ public class StaticDigraphTest {
         Assert.assertEquals(2, flow3);
         Assert.assertEquals(3, flow4);
 
+        long flow1L = algoLong.flow(0);
+        long flow2L = algoLong.flow(1);
+        long flow3L = algoLong.flow(2);
+        long flow4L = algoLong.flow(3);
+
+        Assert.assertEquals(1, flow1L);
+        Assert.assertEquals(0, flow2L);
+        Assert.assertEquals(2, flow3L);
+        Assert.assertEquals(3, flow4L);
+
         algo.close();
+        algoLong.close();
         graph.close();
     }
 }
